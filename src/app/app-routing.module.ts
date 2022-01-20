d2eb9db66419f05bf6f2c9e5c499ecddf59a7f78
eideard-hm <ehhernandez81@misena.edu.co>
2022-01-20 14:26:14 -0500

    Added the module for the routes  settings

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BasicsComponent } from './sales/pages/basics/basics.component';
+import { NumbersComponent } from './sales/pages/numbers/numbers.component';
+import { NoCommonsComponent } from './sales/pages/no-commons/no-commons.component';
+import { OrdenarComponent } from './sales/pages/ordenar/ordenar.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BasicsComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'numeros',
+    component: NumbersComponent
+  },
+  {
+    path: 'no-comunes',
+    component: NoCommonsComponent
+  },
+  {
+    path: 'ordenar',
+    component: OrdenarComponent
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
