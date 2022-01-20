116579e6bfcf458606eb52d528a3350b82af1e66
eideard-hm <ehhernandez81@misena.edu.co>
2022-01-20 16:36:07 -0500

    Cambiar el idioma Ãpor defecto de forma global

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { SalesModule } from './sales/sales.module';
+
+//cambiar el local
+import localES from '@angular/common/locales/es-CO';
+import {registerLocaleData} from '@angular/common';
+
+registerLocaleData(localES);
 
 @NgModule({
   declarations: [
@@ -16,7 +22,12 @@ import { SalesModule } from './sales/sales.module';
     SharedModule,
     SalesModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-CO'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
